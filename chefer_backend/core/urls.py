b1aab52f6f8efb210eee7e779de67c2e38a42161
LilitAsa <lilitasa24@yahoo.com>
2025-05-07 15:57:03 +0300

    installing django 5.2 , craeted project, application and superuser

---
+++ chefer_backend/core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('blog/', blog, name='blog'),
+    path('contact/', contact, name='contact'),
+    path('menu/', menu, name='menu'),
+    path('team/', team, name='team'),
+]
---
+++ chefer_backend/core/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def team(request):
+    return render(request, 'team.html')
+
